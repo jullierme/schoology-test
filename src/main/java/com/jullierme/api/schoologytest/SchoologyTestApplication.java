43bdaf542114b2f3eb19748b18e2f81fc1362ff0
Jullierme Silva Barros <julliermegyn@gmail.com>
2019-11-22 11:44:58 -0300

    first commit

---
+++ src/main/java/com/jullierme/api/schoologytest/SchoologyTestApplication.java
@@ -0,0 +1,13 @@
+package com.jullierme.api.schoologytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoologyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoologyTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jullierme/api/schoologytest/SchoologyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jullierme.api.schoologytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoologyTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
